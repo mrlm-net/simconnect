c66d4c3c1aaacff556ee8f3628fb645fa8004811
Martin Hrášek <martin.hrasek@drmax.eu>
2025-07-10 22:59:39 +0200

    Minor naming corrections

--- pkg/client/debug.go
+++ pkg/client/debug.go
@@ -11,7 +11,7 @@ import (
 
 // SimConnect_RequestResponseTimes requests the response times from the SimConnect server.
 // https://docs.flightsimulator.com/msfs2024/html/6_Programming_APIs/SimConnect/API_Reference/Debug/SimConnect_RequestResponseTimes.htm
-func (e *Engine) SimConnect_RequestResponseTimes(count int) error {
+func (e *Engine) RequestResponseTimes(count int) error {
 	hresult, _, _ := SimConnect_RequestResponseTimes.Call(
 		e.handle,       // hSimConnect (use handle directly, not getHandle())
 		uintptr(count), // Count of response times data values written to respopnse
@@ -25,7 +25,7 @@ func (e *Engine) SimConnect_RequestResponseTimes(count int) error {
 	return nil
 }
 
-func (e *Engine) SimConnect_GetLastSentPacketID(id uintptr) error {
+func (e *Engine) GetLastSentPacketID(id uintptr) error {
 	hresult, _, _ := SimConnect_GetLastSentPacketID.Call(
 		e.handle, // hSimConnect (use handle directly, not getHandle())
 		id,       // Pointer to the packet ID to be filled
