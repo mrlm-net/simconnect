e909dc9724e5d4aad69ee0b305f9cb01e4cbed38
Martin Hrášek <martin.hrasek@drmax.eu>
2025-12-02 19:21:22 +0100

    Extend interface

--- pkg/engine/client.go
+++ pkg/engine/client.go
@@ -4,6 +4,8 @@
 package engine
 
 import (
+	"unsafe"
+
 	"github.com/mrlm-net/simconnect/pkg/types"
 )
 
@@ -24,4 +26,12 @@ type SimConnect interface {
 	GetNextDispatch() (*types.SIMCONNECT_RECV, uint32, error)
 	RequestSystemState(requestID uint32, state types.SIMCONNECT_SYSTEM_STATE) error
 	SubscribeToSystemEvent(eventID uint32, eventName string) error
+	FlightLoad(flightFile string) error
+	FlightPlanLoad(flightPlanFile string) error
+	FlightSave(flightFile string, title string, description string) error
+	RequestDataOnSimObject(requestID uint32, definitionID uint32, objectID uint32, period types.SIMCONNECT_PERIOD, flags types.SIMCONNECT_DATA_REQUEST_FLAG, origin uint32, interval uint32, limit uint32) error
+	RequestDataOnSimObjectType(requestID uint32, definitionID uint32, objectType types.SIMCONNECT_SIMOBJECT_TYPE, period types.SIMCONNECT_PERIOD, flags types.SIMCONNECT_DATA_REQUEST_FLAG, origin uint32, interval uint32, limit uint32) error
+	AddToDataDefinition(definitionID uint32, datumName string, unitsName string, datumType types.SIMCONNECT_DATATYPE, epsilon float32, datumID uint32) error
+	ClearDataDefinition(definitionID uint32) error
+	SetDataOnSimObject(uint32, uint32, types.SIMCONNECT_DATA_SET_FLAG, unsafe.Pointer, uint32) error
 }
