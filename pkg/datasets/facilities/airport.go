68a91355463a529283b9a2b2a1c5d13859c08e60
Martin Hrášek <martin.hrasek@drmax.eu>
2025-12-20 22:43:33 +0100

    Add airport facility dataset

---
+++ pkg/datasets/facilities/airport.go
@@ -0,0 +1,31 @@
+//go:build windows
+// +build windows
+
+package facilities
+
+import "github.com/mrlm-net/simconnect/pkg/datasets"
+
+func NewAirportFacilityDataset() *datasets.FacilityDataSet {
+	return &datasets.FacilityDataSet{
+		Definitions: []datasets.FacilityDataDefinition{
+			"OPEN_AIRPORT",
+			"LATITUDE",
+			"LONGITUDE",
+			"ALTITUDE",
+			"MAGVAR",
+			"NAME",
+			"NAME64",
+			"ICAO",
+			"REGION",
+			"TOWER_LATITUDE",
+			"TOWER_LONGITUDE",
+			"TOWER_ALTITUDE",
+			"TRANSITION_ALTITUDE",
+			"TRANSITION_LEVEL",
+			"IS_CLOSED",
+			"COUNTRY",
+			"CITY_STATE",
+			"CLOSE_AIRPORT",
+		},
+	}
+}
