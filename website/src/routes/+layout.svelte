ba6c0148889d22194ee87a46be61d84021f0fa55
Marty Hrášek <martin.hrasek@gmail.com>
2026-02-19 22:32:06 +0100

    fix(website): remove skip-to-content button from root layout

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -19,11 +19,5 @@
 </script>
 
 <div class="min-h-screen flex flex-col">
-	<a
-		href="#main-content"
-		class="sr-only focus:not-sr-only focus:fixed focus:top-2 focus:left-2 focus:z-50 focus:px-4 focus:py-2 focus:bg-[#58a6ff] focus:text-[#0d1117] focus:rounded focus:font-medium"
-	>
-		Skip to content
-	</a>
 	{@render children()}
 </div>
